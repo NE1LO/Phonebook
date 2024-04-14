eef9e85daf49beab282b00482441dc6a0ea78feb
Anton Smeiko <asmeiko13@gmail.com>
2024-04-14 13:36:01 +0200

    Update selectors.js

--- src/redux/auth/selectors.js
+++ src/redux/auth/selectors.js
@@ -1,4 +1,4 @@
 export const selectIsLoggedIn = (state) => state.auth.isLoggedIn;
 export const selectIsRefreshing = (state) => state.auth.isRefreshing;
 export const selectUser = (state) => state.auth.user;
-export const Token = (state) => state.auth.token;
+export const tokenB = (state) => state.auth.token;

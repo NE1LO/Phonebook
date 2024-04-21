a8552fd131c42009cd3ac47ae9dfdd761f6e7acb
Anton Smeiko <asmeiko13@gmail.com>
2024-04-21 19:17:51 +0200

    Update slice.js

--- src/redux/filters/slice.js
+++ src/redux/filters/slice.js
@@ -2,7 +2,7 @@ import { createSlice } from "@reduxjs/toolkit";
 
 const filterSlice = createSlice({
   name: "filter",
-  initialState: "",
+  initialState: [],
   reducers: {
     setFilter: {
       reducer(_, aciton) {
